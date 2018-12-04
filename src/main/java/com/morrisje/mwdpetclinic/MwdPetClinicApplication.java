334f850bf85de9deef2d31ebcf7fec01b2070337
John Morris <johnmorris@morrisje.com>
2018-12-04 15:13:47 -0500

    Initial Project Setup

---
+++ src/main/java/com/morrisje/mwdpetclinic/MwdPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.morrisje.mwdpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwdPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwdPetClinicApplication.class, args);
+    }
+}
