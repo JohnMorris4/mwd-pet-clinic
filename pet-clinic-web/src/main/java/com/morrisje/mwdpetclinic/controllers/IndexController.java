58dcc4bcb08a9240222bd92fcd562e18de8f0a11
John Morris <johnmorris4@bellsouth.net>
2018-12-08 04:42:21 -0500

    Created index page and controller. closes #8

---
+++ pet-clinic-web/src/main/java/com/morrisje/mwdpetclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.morrisje.mwdpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jmorris on 12/7/18
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "/index.html"})
+    public String index(){
+
+        return "index";
+    }
+
+
+}
