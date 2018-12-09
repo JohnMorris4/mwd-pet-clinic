96a34a43e043eee0f694c1b47323f429aacbe328
John Morris <johnmorris4@bellsouth.net>
2018-12-09 04:42:57 -0500

    Listed all Vets on Index Page. Closes #16

--- pet-clinic-web/src/main/java/com/morrisje/mwdpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/morrisje/mwdpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.morrisje.mwdpetclinic.controllers;
 
+import com.morrisje.mwdpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
