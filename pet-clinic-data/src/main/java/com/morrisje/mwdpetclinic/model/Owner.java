1445adf6ac9da979dadc8e2a7b0facbfb02a8fc4
johnmorris4 <johnmorris@morrisje.com>
2018-12-15 04:50:13 -0500

    Finished setting up Pet, PetType and Visit Entities. Closes #27

--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package com.morrisje.mwdpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jmorris on 12/5/18
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
