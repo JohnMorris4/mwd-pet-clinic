b3d3e512f2f07230d463c0b113440d2f51182c0e
johnmorris4 <johnmorris@morrisje.com>
2018-12-16 05:14:38 -0500

    Created the POJO for Speciality and attached to Vet. Closes #45

--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Vet.java
@@ -1,8 +1,16 @@
 package com.morrisje.mwdpetclinic.model;
-
+import java.util.Set;
 /**
  * Created by jmorris on 12/5/18
  */
 public class Vet extends Person  {
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
