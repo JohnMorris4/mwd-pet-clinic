ecf721e13f795799a88d5c0f4eb9413794364d0d
johnmorris4 <johnmorris@morrisje.com>
2018-12-21 03:53:50 -0500

    Converted Vets over to JPA Mappings. Closes #38

--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Speciality.java
@@ -1,6 +1,15 @@
 package com.morrisje.mwdpetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
