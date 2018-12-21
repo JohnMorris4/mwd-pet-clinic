ef104b98af2fc2a676bcd969fb7eafcdd3fccade
johnmorris4 <johnmorris@morrisje.com>
2018-12-21 03:07:16 -0500

    Setup Base Entity JPA. Closes #36

--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.morrisje.mwdpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by jmorris on 12/7/18
  */
-public class BaseEntity implements Serializable {
 
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
