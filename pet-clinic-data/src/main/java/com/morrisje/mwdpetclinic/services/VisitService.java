0e04345643f41e943656f096e1a39fdcea47f698
johnmorris4 <johnmorris@morrisje.com>
2018-12-21 07:03:13 -0500

    Created tge VisitService and VisitMapService. Closes #48

---
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.morrisje.mwdpetclinic.services;
+
+import com.morrisje.mwdpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package com.morrisje.mwdpetclinic.services.map;
+
+import com.morrisje.mwdpetclinic.model.Visit;
+import com.morrisje.mwdpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
