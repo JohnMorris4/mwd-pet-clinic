bf74b1494f542182acdd9c063394e70096d26f28
johnmorris4 <johnmorris@morrisje.com>
2018-12-21 04:13:29 -0500

    Converted Visits to JPA Mappings. Closes #47

--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.morrisje.mwdpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by jmorris on 12/5/18
@@ -23,6 +25,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -54,5 +59,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
