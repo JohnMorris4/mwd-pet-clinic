fedf27a5f44f38257894d91f6984ed2440f0436a
johnmorris4 <johnmorris@morrisje.com>
2019-01-23 14:06:41 -0500

    Implemented Find owner with new changes. Please check the file closes #56

--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/services/OwnerService.java
@@ -2,12 +2,14 @@ package com.morrisje.mwdpetclinic.services;
 
 import com.morrisje.mwdpetclinic.model.Owner;
 
+import java.util.List;
+
 /**
  * Created by jmorris on 12/7/18
  */
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.morrisje.mwdpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -79,5 +80,12 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+
+        //TODO - Implement later
+        return null;
     }
 }
