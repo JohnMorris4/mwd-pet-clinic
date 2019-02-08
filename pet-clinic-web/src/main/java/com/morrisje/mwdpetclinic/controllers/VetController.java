af947b0e0c8a29c26de0198c7a9e8ad2aec049ae
johnmorris4 <johnmorris@morrisje.com>
2019-02-08 06:34:56 -0500

    Created the Json view for the Vets. Closes #70

--- pet-clinic-web/src/main/java/com/morrisje/mwdpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/morrisje/mwdpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.morrisje.mwdpetclinic.controllers;
 
+import com.morrisje.mwdpetclinic.model.Vet;
 import com.morrisje.mwdpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by jmorris on 12/8/18
@@ -21,5 +26,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
