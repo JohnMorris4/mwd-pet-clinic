ba4b360c0204d26f4e345feba47f888947a29263
johnmorris4 <johnmorris@morrisje.com>
2019-02-08 05:59:00 -0500

    Added DateTimeFormat pattern to the Pet object

--- pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/morrisje/mwdpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.morrisje.mwdpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -46,6 +47,7 @@ public class Pet extends BaseEntity {
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
