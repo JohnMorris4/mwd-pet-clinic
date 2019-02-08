7e5629458bfa6d0c97a47d8001d8cdfc31beebfc
johnmorris4 <johnmorris@morrisje.com>
2019-02-08 05:25:27 -0500

    Wrote test for empty find to return all. Closes #65

--- pet-clinic-web/src/test/java/com/morrisje/mwdpetclinic/controllers/OwnersControllerTest.java
+++ pet-clinic-web/src/test/java/com/morrisje/mwdpetclinic/controllers/OwnersControllerTest.java
@@ -81,6 +81,17 @@ class OwnerControllerTest {
     }
 
     @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
+                        Owner.builder().id(2L).build()));
+        mockMvc.perform(get("/owners").param("lastName", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+    }
+
+    @Test
     void displayOwner() throws Exception {
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
 
